42f45101161c53370ae27815c4e988916b1bcd27
KimTaewan <wwan13@naver.com>
2020-12-16 02:21:27 +0900

    #1.2 Router Setup

---
+++ twitter-clone/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBmekwtYyf6O2zN_lDOTs3yMimRTULSn-Q",
+    authDomain: "twitter-clone-c039d.firebaseapp.com",
+    projectId: "twitter-clone-c039d",
+    storageBucket: "twitter-clone-c039d.appspot.com",
+    messagingSenderId: "762789871480",
+    appId: "1:762789871480:web:6c95b1fd3dfde81749d28d"
+};
+
+export default firebase.initializeApp(firebaseConfig);
