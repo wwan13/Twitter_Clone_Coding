afa8b2b85341711bd5180d610ae1cba978ca967b
KimTaewan <wwan13@naver.com>
2020-12-18 22:57:45 +0900

    add comment

--- twitter-clone/src/components/Router.js
+++ twitter-clone/src/components/Router.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
+// router
 const AppRouter = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
